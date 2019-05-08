92dfe8320a944dd17d6c05e2b95f9b95f94d42df
Felipe PC-Meta <felipe.fernandes@meta.com.br>
2019-05-07 23:15:51 -0300

    adding some weathers informations

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request');
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'https://api.darksky.net/forecast/1bb482599d89905d72ca68ea0fc5333c/'+encodeURIComponent(latitude)+','+encodeURIComponent(longitude);
+    const url = 'https://api.darksky.net/forecast/1bb482599d89905d72ca68ea0fc5333c/'+encodeURIComponent(latitude)+','+encodeURIComponent(longitude)+'?lang=pt&units=si';
 
     request({ url, json: true }, (error, { body }) => { //destruction
         if (error) {
@@ -11,7 +11,7 @@ const forecast = (latitude, longitude, callback) => {
             callback('Unable to find location!', undefined);
             //console.log('Unable to find location!');
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain.');           
+            callback(undefined, body.daily.data[0].summary + ' Temperatura em ' + body.currently.temperature + ' graus Celsius. Temperatura mais alta do dia é de ' + body.daily.data[0].temperatureHigh + ' graus e a mais baixa é de ' + body.daily.data[0].temperatureLow +' graus. Tendo ' + body.currently.precipProbability + '% chance de chuva.');           
         }    
     })
 };
